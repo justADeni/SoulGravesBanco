0753378360d728ed801cf0806b1784a5b266e7d8
justADeni <deninikdo@gmail.com>
2025-09-15 00:26:49 +0200

    feat: full refactor to working example

---
+++ src/main/java/com/github/justadeni/soulGravesBanco/SoulContainer.java
@@ -0,0 +1,62 @@
+package com.github.justadeni.soulGravesBanco;
+
+import dev.faultyfunctions.soulgraves.api.SoulGravesAPI;
+import dev.faultyfunctions.soulgraves.utils.Soul;
+import org.bukkit.inventory.ItemStack;
+import ovh.mythmc.banco.api.storage.BancoContainer;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+public class SoulContainer extends BancoContainer {
+
+    @Override
+    protected Collection<ItemStack> get(UUID uuid) {
+        return SoulGravesAPI.INSTANCE.getPlayerSouls(uuid).stream()
+                .map(Soul::getInventory)
+                .flatMap(List::stream)
+                .toList();
+    }
+
+    @Override
+    protected ItemStack addItem(UUID uuid, ItemStack itemStack) {
+        SoulGravesAPI.INSTANCE.getPlayerSouls(uuid).stream().findFirst()
+                .ifPresent(soul -> {
+                    soul.getInventory().add(itemStack);
+                });
+
+        return null;
+    }
+
+    @Override
+    protected ItemStack removeItem(UUID uuid, ItemStack toRemove) {
+        int remaining = toRemove.getAmount();
+
+        for (Soul soul : SoulGravesAPI.INSTANCE.getPlayerSouls(uuid)) {
+            Iterator<ItemStack> it = soul.getInventory().iterator();
+            while (it.hasNext() && remaining > 0) {
+                ItemStack stack = it.next();
+                if (stack.isSimilar(toRemove)) { // only same type, meta, etc.
+                    if (stack.getAmount() <= remaining) {
+                        remaining -= stack.getAmount();
+                        it.remove(); // fully remove this stack
+                    } else {
+                        stack.setAmount(stack.getAmount() - remaining);
+                        remaining = 0;
+                    }
+                }
+            }
+            if (remaining == 0) break;
+        }
+
+        return remaining == 0 ? null : new ItemStack(toRemove.getType(), remaining);
+    }
+
+    @Override
+    public String friendlyName() {
+        return "SOUL_GRAVE";
+    }
+
+}
--- src/main/java/com/github/justadeni/soulGravesBanco/SoulGravesBanco.java
+++ src/main/java/com/github/justadeni/soulGravesBanco/SoulGravesBanco.java
@@ -1,15 +1,13 @@
 package com.github.justadeni.soulGravesBanco;
 
-import com.github.justadeni.soulGravesBanco.listeners.SoulCreateListener;
-import com.github.justadeni.soulGravesBanco.listeners.SoulDeleteListener;
 import org.bukkit.plugin.java.JavaPlugin;
+import ovh.mythmc.banco.api.Banco;
 
 public class SoulGravesBanco extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new SoulCreateListener(), this);
-        getServer().getPluginManager().registerEvents(new SoulDeleteListener(), this);
+        Banco.get().getStorageRegistry().registerStorage(new SoulContainer());
     }
 
 }
